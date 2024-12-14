7e253e25bc191c58e84950d8b5c87e9abb9d3b07
Rock6982 <rodrigoqm75@gmail.com>
2024-12-14 03:16:07 -0400

    upadte

---
+++ ThreeInRow/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TicTacToe } from './components/TicTacToe'
+
+function App() {
+  return (
+    <div>
+      <h1>Three in a Row</h1>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
